b1a3790fdb295c6c24fbcad405390df4ca640f33
ananditapunj <36956086+ananditapunj@users.noreply.github.com>
2018-03-14 22:51:50 +0100

    Add files via upload

---
+++ SHYBOTS_0603.ino
@@ -0,0 +1,217 @@
+#include "corestats.h"
+
+#define BERT
+#include "personality.h"
+
+#define CHB_DIR 13
+#define CHB_PWM 11
+#define CHB_BRK 8
+#define CHB_SNS A1
+
+#define CHA_DIR 12
+#define CHA_PWM 3
+#define CHA_BRK 9
+#define CHA_SNS A0
+
+#define LDR_Right A3
+#define LDR_Left  A2
+
+// mapping ranges light sensors (thresholds)
+#define LGT_THR_LFT_MIN 180 // lowerbund of the LEFT eye
+#define LGT_THR_LFT_MAX 988
+#define LGT_THR_RGT_MIN 240 // right
+#define LGT_THR_RGT_MAX 990
+
+#define LDR_READ_EVERY_MS 200 //5 times per second
+
+#define VIBRATION_PIN 2
+#define LED_PIN 4
+#define LED_PIN2 5
+
+int motorB_speed = 0;
+
+int BEHAVIOUR_NORMAL = 0;
+int BEHAVIOUR_PARANOID = 1;
+int behaviourMode = BEHAVIOUR_NORMAL;
+
+boolean lightWasShined = false;
+int paranoidCounter = 0;               // Count the light flashes
+
+//RunningAverage avgL( (1000 / LDR_READ_EVERY_MS) * 10 );
+//RunningAverage avgR( (1000 / LDR_READ_EVERY_MS) * 10 );
+
+AdaptiveNormalizer mavgL(0,1);
+AdaptiveNormalizer mavgR(0,1);
+
+int leftEye, rightEye;
+
+//SoftwareSerial mySoftwareSerial(6, 10); // RX, TX // for the mp3  module
+//SoftwareSerial mySoftwareSerial(0, 1); // RX, TX // for the mp3  module
+//DFRobotDFPlayerMini myDFPlayer;
+//void printDetail(uint8_t type, int value);
+
+void init_motor() {
+  // initialize the PWM pins
+  pinMode(CHB_PWM, OUTPUT);
+  digitalWrite(CHB_PWM, LOW);
+  // initialize the PWM pins
+  pinMode(CHA_PWM, OUTPUT);
+  digitalWrite(CHA_PWM, LOW);
+
+  pinMode(CHA_DIR, OUTPUT);
+  pinMode(CHA_BRK, OUTPUT);
+  digitalWrite(CHA_DIR, LOW);
+  digitalWrite(CHA_BRK, LOW);
+  pinMode(CHB_DIR, OUTPUT);
+  pinMode(CHB_BRK, OUTPUT);
+  digitalWrite(CHB_DIR, LOW);
+  digitalWrite(CHB_BRK, LOW);
+
+  Serial.println( "initializing motor" );                                                                                                               
+}
+
+void setup() {
+  // put your setup code here, to run once:
+//  mySoftwareSerial.begin(9600);
+  Serial.begin(115200);
+
+
+  init_motor();
+  //init_audio();
+
+  pinMode(VIBRATION_PIN, OUTPUT);
+  pinMode (LED_PIN, OUTPUT);
+}
+
+void debug_text() {
+  Serial.print("left sensor: ");
+  Serial.println(leftEye);
+  Serial.print("right sensor: ");
+  Serial.println(rightEye);
+}
+
+void debug_graph() {
+//  Serial.print(leftEye);
+//  Serial.print(",");
+//  Serial.print(rightEye);
+//  Serial.print(",");
+  Serial.print( mavgL.get() );
+  Serial.print(",");
+  Serial.println( mavgR.get() );
+}
+
+void read_eyes() {
+
+  rightEye = readEye(LDR_Right);
+  mavgR.put( rightEye );
+
+  leftEye = readEye(LDR_Left);
+  mavgL.put( leftEye );
+}
+void blinking_light () {
+  digitalWrite (LED_PIN,HIGH);
+  delay (2000);
+  digitalWrite (LED_PIN,LOW);
+  delay (2000);
+ // digitalWrite (LED_PIN2, HIGH);
+ // delay (500);
+  //digitalWrite (LED_PIN2, LOW);
+ // delay (2000);
+ // Flasher led for milliseconds 
+  }
+  
+
+void steer_with_light() {
+  analogWrite(CHB_PWM, mappedMotorValue(mavgL.get(), true));
+  analogWrite(CHA_PWM, mappedMotorValue(mavgR.get(), false));
+}
+
+void loop() {
+  debug_graph();
+
+
+  read_eyes();
+
+
+  // normal behaviour:
+  if (behaviourMode == BEHAVIOUR_NORMAL) {
+    steer_with_light();
+    if (mavgR.get() > (0.4*SENSITIVITY) || mavgL.get() > (0.4*SENSITIVITY) ) { // when the room is really dark it should be around 400. Maximun is 1000.
+      shake(true);
+    }
+    else if (mavgR.get() < (0.4*SENSITIVITY) || mavgL.get() < (0.4*SENSITIVITY)) { // if there is no light stops vibrating
+      shake(false);
+      blinking_light ();
+    }
+
+    // check for paranoid light:
+    if (mavgR.get() > (0.6*SENSITIVITY) || mavgL.get() > (0.6*SENSITIVITY)) { 
+      
+      if (lightWasShined==false) {
+        
+        paranoidCounter++;
+        Serial.print("HELP ");
+        Serial.println(paranoidCounter);
+        
+        if (paranoidCounter==3) {
+          behaviourMode = BEHAVIOUR_PARANOID;
+          paranoidCounter = 0;
+        }
+      }
+      lightWasShined = true;
+    } else {
+      lightWasShined = false;
+    }
+    
+  }
+
+  // PARANOID
+  else if (behaviourMode == BEHAVIOUR_PARANOID) { 
+    shake(true);
+    if (random(2)<1) {
+      analogWrite(CHB_PWM, 255); //conect sensor value to direction
+      analogWrite(CHA_PWM, 0);
+    } else {
+      analogWrite(CHB_PWM, 0); //conect sensor value to direction
+      analogWrite(CHA_PWM, 255);
+    }
+    delay(2000);
+    behaviourMode = BEHAVIOUR_NORMAL;
+  }
+  
+  delay(50);
+
+}
+double readEye(int eye) {
+  double reading = .0;
+  // read 4 times to get a stable reading
+  for (int i = 0; i < 4; i++) {
+    reading += analogRead(eye);
+    delay(5);
+  }
+
+  return (reading / 4.0);
+}
+
+
+void shake(bool _shakeOn) {
+  digitalWrite (VIBRATION_PIN, _shakeOn); // if the light is turned on, it activates the vibrating module
+  //  delay (1000);
+  //  digitalWrite (5, LOW);
+  //  delay (1000);
+}
+
+int mappedMotorValue(int reading, bool isLeft) {
+  int mappedValue;
+  if (isLeft) {
+    mappedValue = map(reading, LGT_THR_LFT_MIN, LGT_THR_LFT_MAX, 0, 1023);
+  } else {
+    mappedValue = map(reading, LGT_THR_RGT_MIN, LGT_THR_RGT_MAX, 0, 1023);
+  }
+  mappedValue = mappedValue / 4;
+  if (mappedValue < 0) return 0;
+  // --------------------------
+  if (mappedValue > 255) return 255;
+  return mappedValue;
+}
+
---
+++ flasher.h.ino
@@ -0,0 +1,99 @@
+class Flasher
+{
+  int ledPin;      // the number of the LED pin
+  long OnTime;     // milliseconds of on-time
+  long OffTime;    // milliseconds of off-time
+
+  // These maintain the current state
+  int ledState;                 // ledState used to set the LED
+  unsigned long previousMillis;   // will store last time LED was updated
+
+  bool flashing;
+  unsigned long flashingStarted;
+  unsigned long flashingTime;
+
+  float level;
+  
+  public:
+
+  Flasher(int pin, long on, long off)
+  {
+    ledPin = pin;
+    pinMode(ledPin, OUTPUT);     
+      
+    OnTime = on;
+    OffTime = off;
+    
+    ledState = LOW; 
+    previousMillis = 0;
+  }
+
+  void flash(int howlong = 1000) {
+    if (!flashing) {
+      flashing = true;
+      flashingStarted = millis();
+      flashingTime = howlong;
+    }
+  }
+
+/*
+  void stop() {
+      //flashing = false;
+      digitalWrite(ledPin, LOW);
+  }
+*/
+
+  void reset() {
+    flashing = false;
+    flashingStarted = millis();
+  }
+
+  void debug(){
+    Serial.print(level);
+    Serial.print(", ");
+  }
+  /*
+  float smooth() {
+    return (exp(sin(millis()/(float(OnTime+OffTime))*PI)) - 0.36787944) * 108.0;
+  }
+  float onoff() {
+      float val = .0;
+      unsigned long currentMillis = millis();
+      if((ledState == HIGH) && (currentMillis - previousMillis >= OnTime))
+      {
+        ledState = LOW;  // Turn it off
+        previousMillis = currentMillis;  // Remember the time
+        val = .0;
+      }
+      else if ((ledState == LOW) && (currentMillis - previousMillis >= OffTime))
+      {
+        ledState = HIGH;  // turn it on
+        previousMillis = currentMillis;   // Remember the time
+        val = 255.0;
+      }
+      return val;
+  }
+  */
+  
+  void update()
+  {
+    // check to see if it's time to change the state of the LED
+    //level = smooth();
+
+    if(flashing && (millis()-flashingStarted)<flashingTime) {
+        digitalWrite(ledPin, HIGH);//level);
+    }
+
+    else if(flashing && (millis() - flashingStarted) >= flashingTime ) {
+        //stop();
+        digitalWrite(ledPin, LOW);
+        if ((millis()-flashingStarted)>=flashingTime*2) {
+          reset();  
+        }
+    }
+
+    
+    
+  } // update ()
+
+}; // class
